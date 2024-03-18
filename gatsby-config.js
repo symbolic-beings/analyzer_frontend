36afd0e1af66d1c20c24d60ed49b49e33ec2157f
MaxwellBoecker <boecker.maxwell@gmail.com>
2024-03-17 20:43:06 -0400

    connect to socket server for analysis

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-custom`,
+      options: {
+        socketUrl: process.env.SOCKET_URL,
+      }
+    }
+  ],
 }
--- src/pages/analysisBar.js
+++ src/pages/analysisBar.js
@@ -1,17 +1,31 @@
 import * as React from "react"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import useWebSocket, {ReadyState}from 'react-use-websocket'
 
 const AnalysisBar = ({game}) => {
     // here set up the connection for the web socket
+    // probably put this into component did mount
+    const [socketUrl, setSocketUrl] = useState(process.env.GATSBY_SOCKET_URL);
+    const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+    const [currentAnalysis, setCurrentAnalysis] = useState("");
+    console.log(readyState === ReadyState.CLOSED)
+    
+    useEffect(() => {
+        console.log(lastMessage);
+        if (lastMessage !== null) {
+            setCurrentAnalysis(lastMessage.data);
+        }
+    }, [lastMessage]);
+
     useEffect(()=>{
         // send into websocket for the analysis
-
-        console.log("using effect, to no effect")
+        sendMessage(game.fen());
     }, [game])
 
+
     return (
-        <div>{game.fen()}</div>
+        <div>{currentAnalysis}</div>
     )
 }
 
